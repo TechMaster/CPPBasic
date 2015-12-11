7f19c1ce39d423ae7d1a46ed36f9e2e24a59a0df
Trinh Minh Cuong <cuong@techmaster.vn>
2015-12-11 18:35:11 +0700

    Update Boost DateTime

--- Boost/DateTime/main.cpp
+++ Boost/DateTime/main.cpp
@@ -2,57 +2,70 @@
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/time_zone_base.hpp>
 #include <boost/date_time/local_time/local_time.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
-
 using namespace std;
 using namespace boost::gregorian;
 using namespace boost::posix_time;
 
 void simpleDemo();
-
 void posixTime();
 
 int main() {
-    //simpleDemo();
+    simpleDemo();
     posixTime();
     return 0;
 }
 
 void simpleDemo() {
     date weekstart(2002, Feb, 1);
-    cout << weekstart << endl;
-    date weekend = weekstart + weeks(1);
+    date weekend = weekstart + weeks(2);
+    cout << weekend << endl;
+
     date d1(1975, 11, 27);
-
-    cout << d1 << endl;
-
     date d2 = d1 + days(5);
     cout << d2 << endl;
 
     date today = day_clock::local_day();
     cout << "today " << today << endl;
-    if (d2 >= today) { } //date comparison operators
+    date universalday = day_clock::universal_day();
+
+    date d3 = today + days(-1);
+
+    if (d3 >= today) {
+        cout << "d3 after today" << endl;
+    } else {
+        cout << "d3 before today" << endl;
+    }
 
     date_period thisWeek(today, today + days(5));
-    if (thisWeek.contains(today)) {
-        cout << "This week contains today" << endl;
-    }//do something
 
+    if (thisWeek.contains(today + days(1))) {
+        cout << "This week contains tomorrow" << endl;
+    }
 
     //iterate and print the week
-    day_iterator itr(weekstart);
+    /*day_iterator itr(weekstart);
     while (itr <= weekend) {
         cout << (*itr) << endl;
         ++itr;
-    }
-    //input streaming
-    std::stringstream ss("2004-Jan-1");
-    date d3;
-    ss >> d3;
+    }*/
 
-    //date generator functions
+    //input streaming
+    date dFromString = date_from_iso_string("20140131");
+    cout << dFromString << endl;
+
+    ptime pt(not_a_date_time);
+    stringstream ss("2002-Jan-01 14:23:11");
+    ss >> pt;
+    cout << pt << endl; // "2002-Jan-01 14:23:11"
+
+    //date generator functions. Still buggy
     //date d5 = next_weekday(today, Sunday); //calculate Sunday following d4
     //cout << "next Sunday " << d5 << endl;
+
+    date dA{2014, 1, 21};
+    date dB{2014, 2, 28};
+    date_duration duration = dB - dA;
+    cout << duration.days() << endl;
 
     //US labor day is first Monday in Sept
     typedef nth_day_of_the_week_in_month nth_dow;
@@ -60,7 +73,6 @@ void simpleDemo() {
     //calculate a specific date for 2004 from functor
     date d6 = labor_day.get_date(2015);
     cout << "Labor day in 2015: " << d6 << endl;
-
 }
 
 void posixTime() {
@@ -85,3 +97,4 @@ void posixTime() {
         std::cout << (*titr) << std::endl;
     }
 }
+
